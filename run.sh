932acc630329d76f44003021b3dbcf8484491b60
Cruzer-S <yyyynoom@gmail.com>
2025-07-17 19:11:50 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -6,15 +6,7 @@ PORT=1585
 NQUEUE=1
 INTERFACE=enp12s0f2np2
 
-SERVER=0
+SERVER=1
 DMA=0
-
-if [[ $1 == "server" ]]; then
-	SERVER=1
-fi
-
-if [[ $2 == "dma" ]]; then
-	DMA=1;
-fi
 
 make run ARGUMENTS="$SERVER $ADDRESS $PORT $DMA $NQUEUE $INTERFACE"
