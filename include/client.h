a064b174f408dc7184787c7e31484fe34f4a686f
Cruzer-S <yyyynoom@gmail.com>
2025-08-02 14:56:31 +0900

    brand new start

--- include/client.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLIENT_H__
-#define CLIENT_H__
-
-#include <stdbool.h>
-#include <stddef.h>
-
-void client_start(char *address, int port, bool is_dma, size_t buffer_size, char *ifname);
-
-#endif
